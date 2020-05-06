efd96382fc0fe5c02332ce2a4d4548b88220a68e
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-06 14:09:16 +0000

    Add index function of properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,8 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
+  def index
+    @properties = Property.all
+  end
   def new
     @property = Property.new
   end
